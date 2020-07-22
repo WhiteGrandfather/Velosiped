416e8706b8458f80eb1317b92595921ddcd43b46
Михаил <santa_dj@mail.ru>
2020-07-23 02:36:29 +0300

    Сделал оптимизацию

--- build/js/script.js
+++ build/js/script.js
@@ -3,16 +3,40 @@
 try {
   var navHeader = document.querySelector('.nav');
   var navToggle = document.querySelector('.toggle__button');
+  var navLinksList = navHeader.querySelectorAll('.nav__link');
 
   navHeader.classList.remove('nav--no-js');
+
+  var onLinkClick = function () {
+    if (navHeader.classList.contains('nav--active')) {
+      navHeader.classList.remove('nav--active');
+      navToggle.classList.remove('toggle__button--cross');
+
+      getListener();
+    }
+  };
+
+  var getListener = function (evt) {
+    navLinksList.forEach(function (element) {
+      if (evt === 'add') {
+        element.addEventListener('click', onLinkClick);
+      } else {
+        element.removeEventListener('click', onLinkClick);
+      }
+    });
+  };
 
   navToggle.addEventListener('click', function () {
     if (!navHeader.classList.contains('nav--active')) {
       navHeader.classList.add('nav--active');
       navToggle.classList.add('toggle__button--cross');
+
+      getListener('add');
     } else {
       navHeader.classList.remove('nav--active');
       navToggle.classList.remove('toggle__button--cross');
+
+      getListener();
     }
   });
 } catch (e) {
